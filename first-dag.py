ae91a8ea57c6689dd9c33dfcba9743d76423c9f0
Andrew Rothstein <andrew.rothstein@gmail.com>
2024-12-20 02:22:52 +0000

    no for real

--- first-dag.py
+++ first-dag.py
@@ -4,9 +4,9 @@ from airflow.decorators import dag
 from airflow.operators.empty import EmptyOperator
 
 
-@dag(start_date=datetime.datetime(2021, 1, 1), schedule="@daily")
-def generate_dag():
+@dag(start_date=datetime.datetime(2001, 1, 1), schedule="@daily")
+def generate_dag2():
     EmptyOperator(task_id="another-daily-task")
 
 
-generate_dag()
+generate_dag2()
